28ff0dfb2e0ff57de7543288e7344636f71d0df8
SKvr-46 <koeich4@gmail.com>
2023-03-31 10:31:23 +0900

    commit

--- component/Clock.tsx
+++ component/Clock.tsx
@@ -6,21 +6,56 @@ export const Clock = () => {
     const hourRef = useRef<HTMLParagraphElement>(null)
     const minRef = useRef<HTMLParagraphElement>(null)
     const secRef = useRef<HTMLParagraphElement>(null)
-    const ampmRef = useRef<HTMLParagraphElement>(null)
 
     
     const [hdeg, setHdeg] = useState(0)
     const [mdeg, setMdeg] = useState(0)
     const [sdeg, setSdeg] = useState(0)
 
+    const [day, setDay] = useState("")
+
     useEffect(() => {
         const interval = setInterval(() => {
         
             let date = new Date()
+
+            let wtday = date.getDay()
+            const searchDayfromDate = (wtday: number) => {
+                let dayName = ''
+                switch(wtday) {
+                    case 0:
+                        dayName = 'SUN'
+                        break
+                    case 1:
+                        dayName = 'MON'
+                        break
+                    case 2:
+                        dayName = 'TUE'
+                        break
+                    case 3:
+                        dayName = 'WED'
+                        break
+                    case 4:
+                        dayName = 'THU'
+                        break
+                    case 5:
+                        dayName = 'FRI'
+                        break
+                    case 6:
+                        dayName = 'SAT'
+                        break
+                    default:
+                        dayName = ''
+                }
+                return dayName
+            }  
+
             let h = String(date.getHours()).padStart(2, '0')
             let m = String(date.getMinutes()).padStart(2, '0')
             let s = String(date.getSeconds()).padStart(2, '0')
-    
+
+            setDay(`${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} (${searchDayfromDate(wtday)})`)
+
             setHdeg(360 * date.getHours() / 12 + 45)
             setMdeg(360 * date.getMinutes() / 60 + 45)
             setSdeg(360 * date.getSeconds() / 60 + 45)
@@ -46,6 +81,11 @@ export const Clock = () => {
 
     return (
         <div className={styles.container}>
+            <div className={styles.datecontainer}>
+                <p>
+                    {day}
+                </p>
+            </div>
             <div className={styles.clockcontainer}>
                 <div className={styles.hour}>
                     <div
